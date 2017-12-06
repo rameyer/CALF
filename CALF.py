f2fa9dd11d0b28269d07c35d8c28886bb26b77d4
Romain Meyer <RomainMeyer@eduroam-int-dhcp-97-70-198.ucl.ac.uk>
2017-12-06 11:55:15 +0000

    Modified build with external continuum fitting.

--- CALF.py
+++ CALF.py
@@ -1,22 +1,25 @@
 import numpy as np
-from scipy.optimize import curve_fit
-from scipy.interpolate import UnivariateSpline
+from scipy.optimize import curve_fit, minimize
 from scipy import constants
 from utils import *
 from os import listdir
 from os.path import isfile, join
 import matplotlib.pyplot as plt
-from scipy.signal import argrelmax 
-
 
 ############ user parameters ##################
-window_size =  100
+logN_max = 13
+logN_min = 14
+logN_steps = 11
+
+b_values = [20]
+
+N_CIV_per_strength = 100
+N_CIV_per_QSO = 1
+
+window_size =  40
 z_threshold = 0.0005
-#### Must be < 0.3 if no error profile measure (log10(2)=0.3)
-column_threshold = 0.3
-SNR_threshold = 3
-chi2_spline = 1.3
-N_sigma_detection = 2
+SNR_threshold = 5
+chi2_threshold = 3
 
 ############# Constants #######################
 f_CIV_1548 = 0.194000
@@ -33,116 +36,96 @@ QSO_redshift = {'J0148+0600': 5.923 , 'J0836+0054':5.81 , 'J0927+2001': 5.772,
 	'J0842+1218': 6.069, 'J1048+4637': 6.198 , 'J1137+3549': 6.01 , 'J1148+5251': 6.419 ,
 	'J1509-1749': 6.12, 'J1602+4228': 6.09, 'J2054-0005': 6.062, 'J2315-0023': 6.117 }
 
-all_files = [f for f in listdir('/Users/RomainMeyer/Dropbox/PhD/CIVLya_Corr_QSO/') 
-	if isfile(join('/Users/RomainMeyer/Dropbox/PhD/CIVLya_Corr_QSO/', f))]
-files_spectra = [f for f in all_files if f[11:14]!='gas' and f[-4::]=='.txt']
+###############################################
 
-#for file_spectra in files_spectra:
+all_files = [f for f in listdir('/Users/RomainMeyer/Dropbox/PhD/CIVLya_Corr_QSO/QSOfiles_vpfit/') 
+	if isfile(join('/Users/RomainMeyer/Dropbox/PhD/CIVLya_Corr_QSO/QSOfiles_vpfit/', f))]
+files_spectra = [f for f in all_files if f[-14:-4] == '_continuum' and f[-4::]=='.txt']
 
-file_spectra = files_spectra[13]
-print(file_spectra)
+#files_spectra = [files_spectra[8]]
 
-z_QSO = QSO_redshift[file_spectra[0:10]]	
-spectra = np.loadtxt('../' + file_spectra ,skiprows =1)
+# Fix strength bins, number of CIV per bin, divide by number of QSOs sightlines
+logN = np.linspace(logN_min,logN_max,logN_steps)
+number_loops_QSO = np.ceil(N_CIV_per_strength/len(files_spectra))
 
-lya_index = np.min(np.where(spectra[:,0]>1260*(1+z_QSO)))
+# For each QSO sightlines, for n batches of m CIV
 
-################ Truncate flux, error, wave above LyA #####################
-wave = spectra[:,0]
-med = running_median(spectra[:,1],200)
-f = spectra[:,1]
-error = spectra[:,2]
+Completeness = np.zeros(len(logN))
 
-flux = f - med
+for b in b_values:
+	for strength in logN:
+		matching = 0
+		input_redshifts = []
+		guesses_CIV_file = open('./Completeness/guesses_logN_' + str(strength) + '_b_' +str(b)+'.txt','w')
+		for loop in range(int(number_loops_QSO)):
+			for i in range(len(files_spectra)):
+				print i + loop*len(files_spectra),files_spectra[i]
+				z_QSO = QSO_redshift[files_spectra[i][0:10]]	
+				spectra = np.loadtxt('../QSOfiles_vpfit/' +  files_spectra[i] ,skiprows =2)
+				# Get RESVEL
+				file = open('../QSOfiles_vpfit/' +  files_spectra[i] , 'r')
+				lines= file.readlines()
+				if lines[0][2:7] == 'HIRES':
+					RESVEL = float(lines[1][0:-1][7::])*5
+				else:
+					RESVEL = float(lines[1][0:-1][7::])
+				# Divide by continuum, get RESVEL
+				wave = spectra[:,0]
+				flux = spectra[:,1]
+				err = spectra[:,2]
+				continuum = spectra[:,3]
+				flux = flux/continuum
+				err = err/continuum
+				dvperpix = 1e-3*constants.c*np.array([(wave[i]-wave[i-1])/wave[i] for i in range(len(wave))])
+				dvperpix[0] = dvperpix[1]
+				# Draw random redshifts and fixed strengths from bins
+				#strengths = logN[loop*N_CIV_per_QSO_per_b:np.min((logN_steps,(loop+1)*N_CIV_per_QSO_per_b))]
+				max_noise_index = np.max(np.where(flux/err > 3)) 
+				redshift = np.random.uniform((1+z_QSO)*1026./1216. - 1 , np.max((wave[max_noise_index]/CIV_1-1, z_QSO)),1)
+				amplitude = logN_to_amp(strength,b,(1+redshift)*CIV_1)
+				# Create modified flux
+				mod_flux = add_CIV(wave,flux,redshift,amplitude,b/RESVEL)
+				#print 'Find z=', redshift, ' at ' , (1+redshift)*CIV_1
+				# Get CIV_guesses
+				redshifts_guesses = guess_CIV(wave,mod_flux,err, SNR_threshold = SNR_threshold, 
+											  z_threshold = z_threshold, chi2_threshold = chi2_threshold,
+											  RESVEL = RESVEL, window_size = window_size)
+				
+				input_redshifts.append(redshift)
+				matching += np.round(redshift,2) in np.round(redshifts_guesses,2)
+				guesses_CIV_file.write(str(redshifts_guesses)[1:-1] + '\n')
+				# Write mock CIV redshifts in file A
+		np.savetxt('./Completeness/input_logN_' + str(strength) + '_b_' +str(b)+'.txt',np.array(input_redshifts))
+		Completeness[np.where(logN == strength)] = matching /(number_loops_QSO*len(files_spectra))
+		print Completeness[np.where(logN == strength)]
+np.savetxt('./completeness_'+str(logN_min)+'_' + str(logN_max)+'.txt', Completeness)
 
-error = np.array([e if not(f==0) else np.inf for e,f in zip(error,f) ])
 
 ### dvperpix in km/s
-dvperpix = 1e-3*constants.c*np.array([(wave[i]-wave[i-1])/wave[i] for i in range(len(wave))])
-dvperpix[0] = dvperpix[1]
+#dvperpix = 1e-3*constants.c*np.array([(wave[i]-wave[i-1])/wave[i] for i in range(len(wave))])
+#dvperpix[0] = dvperpix[1]
 
-######################### Gaussian-matched filter  ###########################
+# LogN approx
 
-kernel = gaussian( np.linspace(0,20,20),-0.5, 10,3,0)
+#doublets_full = np.array([[doublet[0],doublet[1],doublet[2],doublet[3],doublet[4]
+#	, np.log10(-np.sum(np.log10(voigt_approx(wave,doublet[0],doublet[1],doublet[2],doublet[3])+1) * dvperpix)
+#		/ (f_CIV_1548 * CIV_1 * column_conversion_const)) ]
+#	for doublet in voigt_params])
 
-SNR = matched_filter(kernel,flux[lya_index::],error[lya_index::])
+#if len(doublets)>0:
+#	print(doublets[:,1]/CIV_1-1.0)
+#	print(doublets[:,2])
+#	print(doublets[:,3])
+#	print(doublets[:,4])
+#	print(doublets[:,5])
 
-#peaks = np.array([w for w,s in zip(wave[lya_index::],SNR) if s > SNR_threshold ])
-peaks_indices = argrelmax(SNR*(SNR>SNR_threshold), order = 5)
-peaks = wave[lya_index::][peaks_indices]
-
-continuum_indices = np.where(SNR < 0.5)
-
-spline = UnivariateSpline(wave[lya_index::][continuum_indices],f[lya_index::][continuum_indices]
-	, w = 1./(error[lya_index::][continuum_indices])**2
-	, k = 3, s = np.sum(1./(error[lya_index::][continuum_indices])**2)*chi2_spline )
-
-
-plt.plot(wave[lya_index::],f[lya_index::],'k')
-plt.plot(wave[lya_index::],spline(wave[lya_index::]), '--c')
-#plt.plot(wave[lya_index::], SNR,'--g')
-plt.show()
-
-f = f / spline(wave)
-error = error /spline(wave)
-
-######################### Fitting gaussians  ###########################
-
-### holder for Gaussian parameters
-gaussian_params = np.zeros((len(peaks),4))
-#### x_0, amp, fhwm_L , fwhm_G
-#voigt_params = np.zeros(len(peaks),4)
-
-
-for i in range(len(peaks)):
-	peak = peaks[i]
-	index_peak = np.where(wave==peak)[0][0]
-	
-	#print(-0.2,peak,1,med[index_peak])
-
-	gaussian_params[i], _  = curve_fit(gaussian,xdata=wave[int(index_peak-window_size*0.5):int(index_peak+window_size*0.5)]
-		, ydata=f[int(index_peak-window_size*0.5):int(index_peak+window_size*0.5)] 
-		, sigma = error[int(index_peak-window_size*0.5):int(index_peak+window_size*0.5)]
-		, p0 =[-0.2,peak,1,1] , bounds = ([-1,peak-5,0.1,1-0.000001],[0,peak+5,3,1]) , method = 'trf')
-	#plt.plot(wave[lya_index::],voigt_approx(wave[lya_index::], *gaussian_params[i]))
-
-#print(gaussian_params)
-
-###################### Selection of doublets ########################## 
-
-pairs_indices = find_potential_doublets(gaussian_params,CIV_1,CIV_2,z_threshold)
-
-for i in pairs_indices:
-	plt.plot(wave[lya_index::],gaussian(wave[lya_index::], *gaussian_params[i[0]]))
-	plt.plot(wave[lya_index::],gaussian(wave[lya_index::], *gaussian_params[i[1]]))
-
-pairs_column_densities = np.array([ [gaussian_params[ind[0],0], gaussian_params[ind[1],0],gaussian_params[ind[0],1] 
-	, gaussian_params[ind[1],1], gaussian_params[ind[0],2], gaussian_params[ind[1],2]
-	, np.log10(-np.sum(np.log10(gaussian(wave,gaussian_params[ind[0],0],gaussian_params[ind[0],1],gaussian_params[ind[0],2],gaussian_params[ind[0],3])/gaussian_params[ind[0],3]) * dvperpix)/ (f_CIV_1548 * CIV_1 * column_conversion_const))
-	, np.log10(-np.sum(np.log10(gaussian(wave,gaussian_params[ind[1],0],gaussian_params[ind[1],1],gaussian_params[ind[1],2],gaussian_params[ind[1],3])/gaussian_params[ind[1],3]) * dvperpix) / (f_CIV_1550 * CIV_2 * column_conversion_const))]  
-	for ind in pairs_indices])
-
-# Apply column density threshold: should be same for CIV 1548 or CIV 1550
-pairs_column_densities = np.array([x for x in pairs_column_densities if np.abs(x[6] - x[7]) < column_threshold])
-# If 1 gaussian can be either CIV 1548 or CIV 1550, prefer CIV 1548
-pairs_column_densities = np.array([x for x in pairs_column_densities if not(x[2] in pairs_column_densities[:,3]) ])
-# Apply N sigma thresholding on CIV 1548 detection
-pairs_column_densities = np.array([x for x in pairs_column_densities if np.abs(x[0]) > N_sigma_detection * error[np.min(np.where(wave>x[2]))] ] )
-print(pairs_column_densities)
-if len(pairs_column_densities)>0:
-	print(pairs_column_densities[:,2]/CIV_1-1.0)
-	print(pairs_column_densities[:,6])
-	print(pairs_column_densities[:,6]-pairs_column_densities[:,7])
-
-###################### Selection of doublets ########################## 
-
-plt.plot(wave[lya_index::],f[lya_index::],'k')
+#plt.plot(wave[lya_index::],f[lya_index::],'k')
 #plt.plot(wave[lya_index::],med[lya_index::],'--b')
-plt.plot(wave[lya_index::],error[lya_index::], '--r')
+#plt.plot(wave[lya_index::],error[lya_index::], '--r')
 #plt.vlines(x = peaks, ymin= 0, ymax = 2, color = 'k')
-plt.vlines(x =  pairs_column_densities[:,2], ymin=0,ymax=3.5, color = 'r')
-plt.vlines(x =  pairs_column_densities[:,3], ymin=0,ymax=3, color = 'g')
-plt.show()
+#plt.vlines(x =  doublets[:,1], ymin=0,ymax=3.5, color = 'r')
+#plt.vlines(x =  doublets[:,1]/CIV_1*CIV_2, ymin=0,ymax=3, color = 'g')
+#plt.show()
 
 
 
--- utils.py
+++ utils.py
@@ -1,35 +1,34 @@
 import numpy as np
-from astropy.modeling.functional_models import Voigt1D
+from numpy.random import uniform
+from scipy import constants
+from scipy.optimize import curve_fit, minimize
+from scipy.interpolate import UnivariateSpline
+import matplotlib.pyplot as plt
+from scipy.signal import argrelmax
 
-def voigt_approx(x,Amp, x0, fwhm_L, fwhm_G):
-	v = Voigt1D(x0,Amp,fwhm_L,fwhm_G)
-	return v(x)
-
-
-def doublet_voigt_approx(x,Amp1,x1,fwhm_L,fwhm_G):
-	'''
-	Since the width/location of doublet is fixed, the profile is 
-	parametrized only by the first feature parameters.
-	'''
+def doublet_CIV_gaussian(x,Amp1,x1,b):
 	f_CIV_1548 = 0.194000
 	f_CIV_1550 = 0.097000
 
 	CIV_1 = 1548.2020    
 	CIV_2 = 1550.7740 
 
-	Amp2 = Amp * f_CIV_1550*CIV_2 / (f_CIV_1548 * CIV_1)
+	Amp2 = Amp1 * f_CIV_1550*CIV_2 / (f_CIV_1548 * CIV_1)
 	x2 = x1/CIV_1 * CIV_2
+	g1 = Amp1*np.exp(-(x-x1)**2/(2*b**2))
+	g2 = Amp2*np.exp(-(x-x2)**2/(2*b**2))
+	
+	return g1+g2
 
-	v1 = voigt_approx(x1,Amp1,fwhm_L,fwhm_G)
-	v2 = voigt_approx(x2,Amp2,fwhm_L,fwhm_G)
+def reduced_chi2_doublet(params,x,flux,error):
+	y = doublet_CIV_gaussian(x,params[0],params[1],params[2])
+	chi2 = np.sum(np.square(y-flux)/np.square(error))
 
-	return v1(x) + v2(x)
-
-#def CIV_doublet(lamba1,amp1,z,sigma, ratio):
+	return chi2/(len(x)-3.) 
 
 def gaussian(x, Amp,mu, sigma, Const):
 	''' A simple gaussian definition with usual parameters'''
-	return Amp*np.exp(-(x-mu)**2/sigma**2) + Const
+	return Amp*np.exp(-(x-mu)**2/(2*sigma**2)) + Const
 
 def matched_filter(u, f, e):
 	''' Matched Filtering: e.g. Hewett 1985, Bolton 2004. 
@@ -51,6 +50,26 @@ def matched_filter(u, f, e):
 	SNR[width: len(f)-width] = Cj1 / np.sqrt(Cj2)
 
 	return SNR
+
+def CIV_matched_filter(wave, flux, err,amp,b):
+	''' Matched Filtering: e.g. Hewett 1985, Bolton 2004. 
+	Inputs:
+	u: kernel
+	f: flux of the spectra
+	e: error
+	Returns:
+	SNR: Signal to noise array for all flux values
+	'''
+	width = 40
+
+	Cj1 = np.array([np.sum(doublet_CIV_gaussian(wave[j-width:j+width],amp,wave[j],b)*flux[j-width:j+width]/err[j-width:j+width]**2)
+		for j in range(len(wave))])
+	Cj2 = np.array([np.sum(doublet_CIV_gaussian(wave[j-width:j+width],amp,wave[j],b)**2/err[j-width:j+width]**2)
+		for  j in range(len(wave))])
+	SNR = Cj1 / np.sqrt(Cj2)
+
+	return SNR
+
 
 def find_pairs(array1, array2, threshold):
 	'''
@@ -77,13 +96,13 @@ def find_potential_doublets(wavelengths, lambda1,lambda2, z_threshold):
 	Return an Nx2 array with indices of the pairs in the original array.
 	'''
 
-	_ , unique_indices = np.unique(np.around(gaussian_params[:,1],1), return_index = True)
+	_ , unique_indices = np.unique(np.around(wavelengths), return_index = True)
 
 	pairs_1 = []
 	pairs_2 = []
 	for i1 in unique_indices:
 		temp_pairs = np.array([[i1,i2] for i2 in unique_indices 
-			if np.abs(gaussian_params[i1,1]/lambda1-gaussian_params[i2,1]/lambda2) < z_threshold])
+			if np.abs(wavelengths[i1]/lambda1-wavelengths[i2]/lambda2) < z_threshold])
 		if temp_pairs != []:
 			pairs_1 = np.concatenate([pairs_1,temp_pairs[:,0]])
 			pairs_2 = np.concatenate([pairs_2,temp_pairs[:,1]])
@@ -91,6 +110,7 @@ def find_potential_doublets(wavelengths, lambda1,lambda2, z_threshold):
 	pairs = np.zeros([len(pairs_1),2])
 	pairs[:,0] = pairs_1
 	pairs[:,1] = pairs_2
+
 
 	return pairs.astype(int)
 
@@ -104,6 +124,89 @@ def running_median(array,width):
 
 	return median
 
+def add_CIV(wave,flux,z,amp,b):
+	CIV_1 = 1548.2020    
+
+	flux_copy = np.copy(flux)
+
+	flux_copy = flux_copy + doublet_CIV_gaussian(wave,amp,CIV_1*(1+z),b)
+
+	return flux_copy
+
+def logN_to_amp(logN,b, wavelengths):
+	###### Constants ######
+	f_CIV_1548 = 0.194000
+	f_CIV_1550 = 0.097000
+	column_conversion_const =  (np.pi*constants.e**2)/(constants.c * constants.m_e) 
+	CIV_1 = 1548.2020    
+	CIV_2 = 1550.7740 
+	###### Conversion #####
+
+	tau = 10**(logN)*(f_CIV_1548 * CIV_1 * column_conversion_const)
 
 
+	return -(1-np.exp(-tau))
+
+def guess_CIV(wave,flux,err, SNR_threshold = 3, z_threshold = 0.0005,
+			  chi2_threshold = 6,RESVEL = 30.0, window_size = 40):
+	redshift_CIV_guesses = []
+	# Constants
+	CIV_1 = 1548.2020    
+	CIV_2 = 1550.7740 
+
+	# Run matched filter
+	kernel = gaussian( np.linspace(0,20,20),-0.2, 10,20/RESVEL,0)
+	SNR = matched_filter(kernel,flux-1,err)
+	#plt.plot(wave,flux)
+	#plt.plot(wave,SNR)
+	#plt.show()
+	peaks_indices = argrelmax(SNR*(SNR>SNR_threshold), order = 5)
+	peaks = wave[peaks_indices]
+	# Call find_potential_doublets
+	pairs_indices = find_potential_doublets(wave[peaks_indices],CIV_1,CIV_2,z_threshold)
+	# Fit the voigt profile and cut in chi^2
+
+	for i in range(len(pairs_indices)):
+		peak = peaks[pairs_indices[i,0]	]
+		index_peak1 = np.where(wave == peak)[0][0]
+		index_peak2 = np.where(wave == peaks[pairs_indices[i,1]	])[0][0]
+		if SNR[index_peak1] <= SNR[index_peak2]:
+			#print 'eliminated pair at z' , peak/CIV_1-1
+			continue
+
+		temp_wave = wave[int(index_peak1-window_size*0.5):int(index_peak2+window_size*0.5)]
+		temp_flux = flux[int(index_peak1-window_size*0.5):int(index_peak2+window_size*0.5)] - 1
+		temp_error = err[int(index_peak1-window_size*0.5):int(index_peak2+window_size*0.5)]
+		res = minimize(fun = reduced_chi2_doublet, x0 = [-0.2,peak,10.0/RESVEL], 
+					   args = (temp_wave,temp_flux, temp_error),
+					   method = 'TNC' , bounds = ( (-1,0), (peak-2,peak+2),
+					   (10/RESVEL,60/RESVEL))  )
+		#print peak/CIV_1-1,reduced_chi2_doublet(res.x,temp_wave,temp_flux, temp_error )
+		if reduced_chi2_doublet(res.x,temp_wave,temp_flux, temp_error ) < chi2_threshold:
+			redshift_CIV_guesses.append(res.x[1]/CIV_1 -1)
+			#print res.x[1]/CIV_1 -1
+			#plt.plot(temp_wave,temp_flux)
+			#plt.plot(temp_wave,doublet_CIV_gaussian(temp_wave, *res.x))
+			#plt.show()
+
+	# Return the redshift of the selected doublets
+	return redshift_CIV_guesses
+
+
+def plot_completeness():
+	# Create array to store completeness
+	# Loop over b,N
+	# find if redshift is matching
+	# count
+	inputs = np.loadtxt('./Completeness/input_logN_13.1_b_20.txt')
+	guesses = np.loadtxt('./Completeness/guesses_logN_13.1_b_20.txt')
+	print len(guesses), len(inputs)
+
+
+
+	
+
+
+
+	
 
