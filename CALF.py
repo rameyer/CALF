95edd23445e2e6d12156158e9d2c8fbfa6278380
Romain Meyer <RomainMeyer@eduroam-int-dhcp-97-5-181.ucl.ac.uk>
2017-12-11 14:10:07 +0000

    Stable with increasing completeness.

--- CALF.py
+++ CALF.py
@@ -7,17 +7,17 @@ from os.path import isfile, join
 import matplotlib.pyplot as plt
 
 ############ user parameters ##################
-logN_max = 13
-logN_min = 14
-logN_steps = 11
+logN_max = 13.2
+logN_min = 13.2
+logN_steps = 1
 
 b_values = [20]
 
 N_CIV_per_strength = 100
 N_CIV_per_QSO = 1
 
-window_size =  40
-z_threshold = 0.0005
+window_size =  30
+z_threshold = 0.001
 SNR_threshold = 5
 chi2_threshold = 3
 
@@ -40,9 +40,9 @@ QSO_redshift = {'J0148+0600': 5.923 , 'J0836+0054':5.81 , 'J0927+2001': 5.772,
 
 all_files = [f for f in listdir('/Users/RomainMeyer/Dropbox/PhD/CIVLya_Corr_QSO/QSOfiles_vpfit/') 
 	if isfile(join('/Users/RomainMeyer/Dropbox/PhD/CIVLya_Corr_QSO/QSOfiles_vpfit/', f))]
-files_spectra = [f for f in all_files if f[-14:-4] == '_continuum' and f[-4::]=='.txt']
+files_spectra = [f for f in all_files if f[-16:-4] == '_continuum_X' and f[-4::]=='.txt']
 
-#files_spectra = [files_spectra[8]]
+#files_spectra =files_spectra[::-1]
 
 # Fix strength bins, number of CIV per bin, divide by number of QSOs sightlines
 logN = np.linspace(logN_min,logN_max,logN_steps)
@@ -59,7 +59,7 @@ for b in b_values:
 		guesses_CIV_file = open('./Completeness/guesses_logN_' + str(strength) + '_b_' +str(b)+'.txt','w')
 		for loop in range(int(number_loops_QSO)):
 			for i in range(len(files_spectra)):
-				print i + loop*len(files_spectra),files_spectra[i]
+				print(i + loop*len(files_spectra),files_spectra[i])
 				z_QSO = QSO_redshift[files_spectra[i][0:10]]	
 				spectra = np.loadtxt('../QSOfiles_vpfit/' +  files_spectra[i] ,skiprows =2)
 				# Get RESVEL
@@ -70,34 +70,36 @@ for b in b_values:
 				else:
 					RESVEL = float(lines[1][0:-1][7::])
 				# Divide by continuum, get RESVEL
-				wave = spectra[:,0]
-				flux = spectra[:,1]
-				err = spectra[:,2]
-				continuum = spectra[:,3]
+				wave = np.array([w for w,f in zip(spectra[:,0],spectra[:,1]) if f != 0.0])
+				flux = np.array([f for f in spectra[:,1] if f != 0.0])
+				err = np.array([e for e,f in zip(spectra[:,2],spectra[:,1]) if f != 0.0])
+				continuum = np.array([c for c,f in zip(spectra[:,3],spectra[:,1]) if f != 0.0])
 				flux = flux/continuum
 				err = err/continuum
 				dvperpix = 1e-3*constants.c*np.array([(wave[i]-wave[i-1])/wave[i] for i in range(len(wave))])
 				dvperpix[0] = dvperpix[1]
 				# Draw random redshifts and fixed strengths from bins
 				#strengths = logN[loop*N_CIV_per_QSO_per_b:np.min((logN_steps,(loop+1)*N_CIV_per_QSO_per_b))]
-				max_noise_index = np.max(np.where(flux/err > 3)) 
-				redshift = np.random.uniform((1+z_QSO)*1026./1216. - 1 , np.max((wave[max_noise_index]/CIV_1-1, z_QSO)),1)
+				max_noise_index = np.max(np.where(err < 0.2)) 
+				#print(wave[max_noise_index] / (1+z_QSO)), np.min((np.max(wave)/(CIV_2+10)-1, z_QSO))
+				redshift = np.random.uniform((1+z_QSO)*1026./1216. - 1 , np.min((np.max(wave[max_noise_index])/(CIV_2+10)-1, z_QSO)),1)
 				amplitude = logN_to_amp(strength,b,(1+redshift)*CIV_1)
 				# Create modified flux
-				mod_flux = add_CIV(wave,flux,redshift,amplitude,b/RESVEL)
-				#print 'Find z=', redshift, ' at ' , (1+redshift)*CIV_1
+				mod_flux = add_CIV(wave,flux,redshift,amplitude,b/np.mean(dvperpix))
+				print( 'Find z=', redshift, ' at ' , (1+redshift)*CIV_1)
 				# Get CIV_guesses
 				redshifts_guesses = guess_CIV(wave,mod_flux,err, SNR_threshold = SNR_threshold, 
 											  z_threshold = z_threshold, chi2_threshold = chi2_threshold,
-											  RESVEL = RESVEL, window_size = window_size)
+											  RESVEL = np.mean(dvperpix), window_size = window_size)
 				
 				input_redshifts.append(redshift)
+				print(np.round(redshift,2) in np.round(redshifts_guesses,2))
 				matching += np.round(redshift,2) in np.round(redshifts_guesses,2)
 				guesses_CIV_file.write(str(redshifts_guesses)[1:-1] + '\n')
 				# Write mock CIV redshifts in file A
 		np.savetxt('./Completeness/input_logN_' + str(strength) + '_b_' +str(b)+'.txt',np.array(input_redshifts))
 		Completeness[np.where(logN == strength)] = matching /(number_loops_QSO*len(files_spectra))
-		print Completeness[np.where(logN == strength)]
+		print(Completeness[np.where(logN == strength)])
 np.savetxt('./completeness_'+str(logN_min)+'_' + str(logN_max)+'.txt', Completeness)
 
 
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from scipy.optimize import curve_fit, minimize
 from scipy.interpolate import UnivariateSpline
 import matplotlib.pyplot as plt
 from scipy.signal import argrelmax
+from scipy.stats import sigmaclip
 
 def doublet_CIV_gaussian(x,Amp1,x1,b):
 	f_CIV_1548 = 0.194000
@@ -26,9 +27,16 @@ def reduced_chi2_doublet(params,x,flux,error):
 
 	return chi2/(len(x)-3.) 
 
-def gaussian(x, Amp,mu, sigma, Const):
+def gaussian(x, Amp,mu, sigma):
 	''' A simple gaussian definition with usual parameters'''
-	return Amp*np.exp(-(x-mu)**2/(2*sigma**2)) + Const
+	return Amp*np.exp(-(x-mu)**2/(2*sigma**2)) 
+
+def reduced_chi2_gaussian(params,x,flux,error):
+	y =  params[0]*np.exp(-(x-params[1])**2/(2*params[2]**2)) 
+
+	chi2 = np.sum(np.square(y-flux)/np.square(error))
+
+	return chi2/(len(x)-3)
 
 def matched_filter(u, f, e):
 	''' Matched Filtering: e.g. Hewett 1985, Bolton 2004. 
@@ -147,23 +155,126 @@ def logN_to_amp(logN,b, wavelengths):
 
 	return -(1-np.exp(-tau))
 
+def fit_sigma_clip(wave, flux, error,
+				   sigma = 2, iters = 3,fun = reduced_chi2_doublet, x0 = [-0.2,1e4,0.1], 
+				   method = 'TNC' , bounds = ( (-1,0), (1e4-2,1e4+2),(5,60))  ):
+		
+		temp_wave = wave
+		temp_flux = flux
+		temp_error = error
+		init = x0
+
+		for i in range(iters):
+			res = minimize(fun = reduced_chi2_doublet, x0 = init, 
+					   args = (temp_wave,temp_flux, temp_error),
+					   method = method , bounds = bounds)
+			fit_flux = doublet_CIV_gaussian(temp_wave,*res.x)
+			#plt.plot(temp_wave,fit_flux)
+			#plt.plot(temp_wave,temp_flux)
+			#plt.show()
+
+			std = np.std(fit_flux-temp_flux)
+			new_indices = [k for k in range(len(temp_wave)) if np.abs(temp_flux[k]-fit_flux[k]) < sigma*std]
+			if new_indices == []:
+				continue
+			else:
+				new_indices = np.array(new_indices)
+				#print (i,new_indices)
+				temp_wave = temp_wave[new_indices]
+				temp_flux = temp_flux[new_indices]
+				temp_error = temp_error[new_indices]
+				init = res.x
+		return res,temp_wave,temp_flux,temp_error
+
+def logN(wave,flux,err,delta_width, window_size = 40):
+	###### Constants ######
+	f_CIV_1548 = 0.194000
+	f_CIV_1550 = 0.097000
+	column_conversion_const =  (np.pi*constants.e**2)/(constants.c * constants.m_e) 
+	CIV_1 = 1548.2020    
+	CIV_2 = 1550.7740 
+	###### Conversion #####
+	logN = np.zeros(len(wave))
+	dvperpix = 1e-3*constants.c*np.array([(wave[i]-wave[i-1])/wave[i] for i in range(len(wave))])
+	dvperpix[0] = dvperpix[1]
+	RESVEL = np.mean(dvperpix)
+	step_pixel = int(np.max((np.ceil(delta_width/np.mean(dvperpix)),1)))
+	#wave_bin = wave[range(window_size,len(wave)-window_size,step_pixel)]
+	logN_1 = np.zeros(len(wave))
+	logN_2 = np.zeros(len(wave))
+	
+	for i in range(window_size,len(wave)-window_size,step_pixel):
+		temp_wave = wave[int(i-window_size*0.5):int(i+window_size*0.5)]
+		temp_flux = flux[int(i-window_size*0.5):int(i+window_size*0.5)] -1
+		temp_err = err[int(i-window_size*0.5):int(i+window_size*0.5)]
+		res,_,_,_ = fit_sigma_clip(temp_wave,temp_flux, temp_err, 
+			   iters = 5, sigma = 2,fun = reduced_chi2_gaussian, x0 = [-0.2,wave[i],20.0/RESVEL], 
+			   method = 'TNC' , bounds = ( (-1,0),(wave[i]-1e-15,wave[i]), (5/RESVEL,40/RESVEL))  )
+
+		tau = -np.log(1+res.x[0])
+		if (1+res.x[0]) > 3*err[i]:
+			integral_tau = -res.x[0]*np.sqrt(2*np.pi)*res.x[2]
+			#print tau, int_tau
+			logN_1[i] = -np.log10((f_CIV_1548 * CIV_1 * column_conversion_const) /integral_tau)
+			logN_2[i] = -np.log10((f_CIV_1550 * CIV_2 * column_conversion_const) /integral_tau)
+
+
+	wave_c = np.array([w for w,c in zip(wave,logN_1) if c != 0])
+	logN_1 = np.array([lN for lN in logN_1 if lN != 0.0])
+	logN_2 = np.array([lN for lN in logN_2 if lN != 0.0])
+	return logN_1,logN_2,wave_c
+
+def potential_CIV_from_column_densities(wave_c,columns_1,columns_2,threshold_columns = 12.5,
+								 tol_columns = 0.05, tol_redshift = 0.001):
+	# Constants
+	CIV_1 = 1548.2020    
+	CIV_2 = 1550.7740
+
+	redshifts = []
+
+	#local_max_indices = argrelmax(columns_1*(columns_1>threshold_columns), order = 5)
+	#thresholded_columns_1 = np.array(zip(columns_1[local_max_indices],wave_c[local_max_indices]))
+	thresholded_columns_1 = np.array([[c1,w]  for c1,w in zip(columns_1,wave_c) if c1 > threshold_columns])
+	for tc1 in thresholded_columns_1:
+		potential_columns = np.array([c2 for c2,w2 in zip(columns_2,wave_c) 
+				if (np.abs(tc1[1]/CIV_1-w2/CIV_2) < tol_redshift and
+					np.abs(tc1[0] - c2) < tol_columns) ])
+		if len(potential_columns)>0:
+			redshifts.append(tc1[1]/CIV_1 - 1)
+	return redshifts
+
 def guess_CIV(wave,flux,err, SNR_threshold = 3, z_threshold = 0.0005,
-			  chi2_threshold = 6,RESVEL = 30.0, window_size = 40):
+				chi2_threshold = 6,RESVEL = 30.0, window_size = 20):
 	redshift_CIV_guesses = []
 	# Constants
 	CIV_1 = 1548.2020    
-	CIV_2 = 1550.7740 
+	CIV_2 = 1550.7740
+	# Columns_1
+	columns_1, columns_2,wave_c = logN(wave,flux,err,delta_width = 20,window_size=window_size)
+	# Find paired densities + thresholding
+	CIV_redshifts = potential_CIV_from_column_densities(wave_c,columns_1,columns_2,
+						threshold_columns = 13,tol_columns = 0.1, tol_redshift = 0.001)
+	print CIV_redshifts
 
+	#plt.plot(wave_c, columns_1)
+	#plt.plot(wave_c, columns_2)
+	#plt.plot(wave,flux)
+	#plt.scatter((1+np.array(CIV_redshifts))*CIV_1, np.ones(len(CIV_redshifts))*1.5)
+	#plt.show()
+	return CIV_redshifts	
+	'''
 	# Run matched filter
 	kernel = gaussian( np.linspace(0,20,20),-0.2, 10,20/RESVEL,0)
 	SNR = matched_filter(kernel,flux-1,err)
 	#plt.plot(wave,flux)
 	#plt.plot(wave,SNR)
 	#plt.show()
-	peaks_indices = argrelmax(SNR*(SNR>SNR_threshold), order = 5)
+	peaks_indices = argrelmax(*(SNR>SNR_threshold), order = 5)
 	peaks = wave[peaks_indices]
+	#print peaks
 	# Call find_potential_doublets
 	pairs_indices = find_potential_doublets(wave[peaks_indices],CIV_1,CIV_2,z_threshold)
+	print peaks[pairs_indices[:,0]]
 	# Fit the voigt profile and cut in chi^2
 
 	for i in range(len(pairs_indices)):
@@ -177,21 +288,25 @@ def guess_CIV(wave,flux,err, SNR_threshold = 3, z_threshold = 0.0005,
 		temp_wave = wave[int(index_peak1-window_size*0.5):int(index_peak2+window_size*0.5)]
 		temp_flux = flux[int(index_peak1-window_size*0.5):int(index_peak2+window_size*0.5)] - 1
 		temp_error = err[int(index_peak1-window_size*0.5):int(index_peak2+window_size*0.5)]
-		res = minimize(fun = reduced_chi2_doublet, x0 = [-0.2,peak,10.0/RESVEL], 
-					   args = (temp_wave,temp_flux, temp_error),
+		res,temp_wave,temp_flux,temp_error = fit_sigma_clip(temp_wave,temp_flux, temp_error, 
+					   iters = 3, sigma = 2,fun = reduced_chi2_doublet, x0 = [-0.2,peak,20.0/RESVEL], 
 					   method = 'TNC' , bounds = ( (-1,0), (peak-2,peak+2),
-					   (10/RESVEL,60/RESVEL))  )
+					   (5/RESVEL,60/RESVEL))  )
+		#res = minimize(fun = reduced_chi2_doublet, x0 = [-0.2,peak,20.0/RESVEL], 
+		#			   args = (temp_wave,temp_flux, temp_error),
+		#			   method = 'TNC' , bounds = ( (-1,0), (peak-2,peak+2),
+		#			   (5/RESVEL,60/RESVEL))  )
 		#print peak/CIV_1-1,reduced_chi2_doublet(res.x,temp_wave,temp_flux, temp_error )
 		if reduced_chi2_doublet(res.x,temp_wave,temp_flux, temp_error ) < chi2_threshold:
 			redshift_CIV_guesses.append(res.x[1]/CIV_1 -1)
-			#print res.x[1]/CIV_1 -1
+			#print(res.x[1]/CIV_1 -1)
 			#plt.plot(temp_wave,temp_flux)
 			#plt.plot(temp_wave,doublet_CIV_gaussian(temp_wave, *res.x))
 			#plt.show()
 
 	# Return the redshift of the selected doublets
 	return redshift_CIV_guesses
-
+	'''
 
 def plot_completeness():
 	# Create array to store completeness
@@ -200,7 +315,7 @@ def plot_completeness():
 	# count
 	inputs = np.loadtxt('./Completeness/input_logN_13.1_b_20.txt')
 	guesses = np.loadtxt('./Completeness/guesses_logN_13.1_b_20.txt')
-	print len(guesses), len(inputs)
+	print(len(guesses), len(inputs))
 
 
 
@@ -209,4 +324,3 @@ def plot_completeness():
 
 
 	
-
